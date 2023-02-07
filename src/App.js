87e465b83bb39c73f7729a65460d20f5f1e4a219
kcheykim <kosalcheykim@gmail.com>
2023-02-06 22:35:55 -0800

    adding files to src

--- src/App.js
+++ src/App.js
@@ -1,6 +1,18 @@
-import React from 'react';
+import { useEffect } from 'react';
+import './App.css';
+import searchIcon from './search.svg';
 
+const API_URL = 'http://www.omdbapi.com?apikey=1cc6fec0'
 const App = () => {
+    const searchMovies = async (title) => {
+        const response = await fetch(`${API_URL}&s=${title}`);
+        const data = await response.json();
+
+        console.log(data.Search);
+    }
+    useEffect(() =>{
+        searchMovies('Spiderman');
+    }, []);
     return (
         <h1>App</h1>
     );
